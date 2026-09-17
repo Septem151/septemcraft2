8c70a747164f84dfde517a7e68db7e29c7d5bf87
Carly Mullins <carly.mullins+github@pm.me>
2026-09-17 05:47:07 -0500

    rename

---
+++ src/main/java/io/gifsync/septempack/SeptemPackConfig.java
@@ -0,0 +1,8 @@
+package io.gifsync.septempack;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod.EventBusSubscriber(modid = SeptemPackMod.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class SeptemPackConfig
+{
+}
